708bf54ce3aceea43a9ba5eb2981a1aefee16211
kimjh96 <kimjhs@kakao.com>
2021-10-27 01:06:13 +0900

    feat: Brand Assets 에 따른 color 정의

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -1,5 +1,28 @@
 export default {
-  color: {
-    primary: 'rgb(99, 137, 176)'
+  palette: {
+    primary: '#8490FF',
+    secondary: '#6BE9FF',
+    neutral: 'rgba(255, 255, 255, 0.08)',
+    mouseover: 'rgba(255, 255, 255, 0.05)',
+    click: 'rgba(255, 255, 255, 0.12)',
+    disabled: 'rgba(255, 255, 255, 0.04)',
+    redEmphasize: '#F83232'
+  },
+  typography: {
+    color: {
+      default: '#FFFFFF',
+      mouseover: 'rgba(255, 255, 255, 0.9)',
+      click: 'rgba(255, 255, 255, 0.9)',
+      disabled: 'rgba(255, 255, 255, 0.3)'
+    }
+  },
+  box: {
+    color: {
+      default: 'rgba(255, 255, 255, 0.08)',
+      mouseover: 'rgba(255, 255, 255, 0.05)',
+      click: 'rgba(255, 255, 255, 0.12)',
+      border: 'rgba(255, 255, 255, 0.12)',
+      disabled: 'rgba(255, 255, 255, 0.04)'
+    }
   }
 };
--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,5 +1,28 @@
 export default {
-  color: {
-    primary: '#8EC5FC'
+  palette: {
+    primary: '#8490FF',
+    secondary: '#6BE9FF',
+    neutral: '#7CAFFF',
+    mouseover: '#99C1FF',
+    click: '#6E9FED',
+    disabled: '#A5C8FF',
+    redEmphasize: '#F83232'
+  },
+  typography: {
+    color: {
+      default: '#323232',
+      mouseover: 'rgba(50, 50, 50, 0.9)',
+      click: 'rgba(50, 50, 50, 0.9)',
+      disabled: 'rgba(50, 50, 50, 0.3)'
+    }
+  },
+  box: {
+    color: {
+      default: 'rgba(50, 50, 50, 0.05)',
+      mouseover: 'rgba(50, 50, 50, 0.03)',
+      click: 'rgba(50, 50, 50, 0.1)',
+      border: 'rgba(50, 50, 50, 0.1)',
+      disabled: 'rgba(50, 50, 50, 0.02)'
+    }
   }
 };
